876f709960c4cb0df0e0a9ade304cc3bb48a9992
khurshed <hurshed@gmail.com>
2023-07-11 01:17:37 +0500

    Добавил решение задачи, которая вводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая вводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+int GetRandomNumber()
+{
+    int number = new Random().Next(10, 100);
+    Console.WriteLine(number);
+    return number;
+}
+
+int GetFirstDigit(int number1)
+{
+    number1 = number1 / 10;
+    Console.WriteLine(number1);
+    return number1;
+}
+
+int GetSecondDigit(int number2)
+{
+    number2 = number2 % 10;
+    Console.WriteLine(number2);
+    return number2;
+}
+
+int num = GetRandomNumber();
+int firstDigit = GetFirstDigit(num);
+int secondDigit = GetSecondDigit(num);
+
+
+
+

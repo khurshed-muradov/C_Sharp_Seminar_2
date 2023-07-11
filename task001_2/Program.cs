e422ddbfba6cc5f61021ab99bfc79ad02c02b319
khurshed <hurshed@gmail.com>
2023-07-11 21:51:03 +0500

    Добавил решение задачи

---
+++ task001_2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая вводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+int GetRandomNumber()
+{
+    int number = new Random().Next(10, 100);
+    return number;
+}
+
+int GetFirstDigit(int number1)
+{
+    return number1 / 10;
+}
+
+int GetLastDigit(int number1)
+{
+    return number1 % 10;
+}
+
+void PrintNumber(int num1)
+{
+    Console.Write(num1 + " ");
+}
+
+void ShowLargestDigit(int digit1, int digit2)
+{
+    if (digit1 > digit2)
+    {
+        Console.WriteLine("-> " + digit1);
+    }
+    else Console.WriteLine("-> " + digit2);
+}
+
+int num = GetRandomNumber();
+PrintNumber(num);
+int firstDigit = GetFirstDigit(num);
+// PrintNumber(firstDigit);
+int lastDigit = GetLastDigit(num);
+// PrintNumber(lastDigit);
+ShowLargestDigit(firstDigit, lastDigit);
+
+
+int num2 = GetRandomNumber();
+PrintNumber(num2);
+int firstDigit2 = GetFirstDigit(num2);
+int lastDigit2 = GetLastDigit(num2);
+ShowLargestDigit(firstDigit2, lastDigit2);
+
+int num3 = GetRandomNumber();
+PrintNumber(num3);
+int firstDigit3 = GetFirstDigit(num3);
+int lastDigit3 = GetLastDigit(num3);
+ShowLargestDigit(firstDigit3, lastDigit3);

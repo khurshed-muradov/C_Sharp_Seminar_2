19c991ecfb8cc5ebf38c2068b41d7e83f3e1d933
khurshed <hurshed@gmail.com>
2023-07-12 00:38:49 +0500

    Save 002

--- S3_task002/Program.cs
+++ S3_task002/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+string mess = "Введите число: ";
+int number1 = Prompt(mess);
+int number2 = Prompt("Введите пятизначное число");
+
+int result1 = FindThirdNumber(number1);
+int result2 = FindThirdNumber(number2);
+
+GetResult(result1);
+GetResult(result2);
+
+
+
+void GetResult(int res)
+{
+    if (res == -1)
+    {
+        Console.WriteLine("Такой цифры нет! ");
+    }
+    else
+    {
+        Console.WriteLine($"Третья цифра числа {res}");
+    }
+}
+
+
+
+int Prompt(string message)
+{
+    Console.Clear();
+    Console.WriteLine(message);
+    int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    return num;
+}
+
+int FindThirdNumber(int num)
+{
+    if (num < 100)
+    {
+        return -1;
+
+    }
+    else
+    {
+        while (num >= 1000)
+        {
+            num = num / 10;
+        }
+        int thirdNumber = num % 10;
+        return thirdNumber;
+    }
+}
